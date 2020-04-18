9564b5d68bbf34c3399ec9ba7484ad8386d73d89
SamundraShahi <samundrashahi@gmail.com>
2020-04-18 21:11:09 +1000

    Toast message

--- app/src/main/java/com/example/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/geoquiz/QuizActivity.java
@@ -2,7 +2,9 @@ package com.example.geoquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
@@ -14,5 +16,36 @@ public class QuizActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        // FOR TRUE BUTTON
+        // getting the reference of widgets
+        mTrueButton = (Button) findViewById(R.id.true_button);
+
+        // this listens/waits
+        // when button is pressed, it listens
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // show message when clicked
+                Toast.makeText(QuizActivity.this,
+                        R.string.correct_toast,
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        //FOR FALSE BUTTON
+        mFalseButton = (Button) findViewById(R.id.false_button);
+
+        // onclick listener here
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //show incorrect message when clicked
+                Toast.makeText(QuizActivity.this,
+                        R.string.incorrect_toast,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
