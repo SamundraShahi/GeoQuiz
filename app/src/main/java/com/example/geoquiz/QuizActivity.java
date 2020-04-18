38d1919386d3ee048d41cafb3e0385d69e245a0e
SamundraShahi <samundrashahi@gmail.com>
2020-04-18 21:18:35 +1000

    this

--- app/src/main/java/com/example/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/geoquiz/QuizActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class QuizActivity extends AppCompatActivity {
 
+    // just some changes
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
