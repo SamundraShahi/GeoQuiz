363e858ab08748e501e5a04a8b42e3ca133a1994
SamundraShahi <samundrashahi@gmail.com>
2020-04-18 19:19:02 +1000

    added some lines

--- app/src/main/java/com/example/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/geoquiz/QuizActivity.java
@@ -1,10 +1,13 @@
 package com.example.geoquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     // this is created when instance of activity class is created.
     @Override
